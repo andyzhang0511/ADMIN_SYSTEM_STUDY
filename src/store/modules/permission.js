85982f9ef2c2c60d21a7521a0b9c771070188a37
zhangfan <zhangfan0511>
2020-04-25 18:33:05 +0800

    '4.25'

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -22,7 +22,7 @@ export function filterAsyncRoutes(routes, roles) {
   const res = []
   routes.forEach(route => {
     const tmp = { ...route } // 解构
-    console.log(tmp)
+    // console.log(tmp)
     if (hasPermission(roles, tmp)) {
       if (tmp.children) {
         tmp.children = filterAsyncRoutes(tmp.children, roles)
@@ -52,8 +52,8 @@ const actions = {
       let accessedRoutes
       if (roles.includes('admin')) { // 如果是admin账户的话就拥有所有的异步路由权限
         accessedRoutes = asyncRoutes || []
-        console.log(asyncRoutes) // 18个异步路由
-        console.log(accessedRoutes) // 18个异步路由
+        // console.log(asyncRoutes) // 18个异步路由
+        // console.log(accessedRoutes) // 18个异步路由
       } else { // 如果不是admin账户的话,就要做权限过滤处理
         accessedRoutes = filterAsyncRoutes(asyncRoutes, roles)
       }
--- src/utils/request.js
+++ src/utils/request.js
@@ -3,49 +3,49 @@ import { MessageBox, Message } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
 
-// create an axios instance
+// 使用自定义配置新建一个 axios 实例 axios.create([config])
+// 自己的项目 {NODE_ENV: "development", VUE_APP_BASE_API: "/admin", BASE_URL: "/"}
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  // withCredentials: true, // send cookies when cross-domain requests
-  timeout: 5000 // request timeout
+  timeout: 5000 // 如果请求话费了超过 `timeout` 的时间，请求将被中断
 })
-
-// request interceptor
+// 请求拦截器
 service.interceptors.request.use(
   config => {
-    // do something before request is sent
-
+    // 在发送请求之前做一些事情
     if (store.getters.token) {
-      // let each request carry token
-      // ['X-Token'] is a custom headers key
-      // please modify it according to the actual situation
-      config.headers['X-Token'] = getToken()
+      /**
+       * 让每个请求都带上token
+       * ['X-Token']是自定义头密钥
+       * 请根据实际情况修改
+       */
+      config.headers['X-Token'] = getToken() // token根据实际要求设置(后端传回token给前端)
     }
     return config
   },
   error => {
-    // do something with request error
-    console.log(error) // for debug
+    // 处理请求错误
+    console.log(error)
     return Promise.reject(error)
   }
 )
 
-// response interceptor
+// 响应拦截器
 service.interceptors.response.use(
   /**
-   * If you want to get http information such as headers or status
+   * 如果您想获取诸如头或状态之类的http信息
    * Please return  response => response
   */
 
   /**
-   * Determine the request status by custom code
-   * Here is just an example
-   * You can also judge the status by HTTP Status Code
+   * 通过自定义代码确定请求状态
+   * 这里只是一个例子
+   * 您还可以通过HTTP状态代码判断状态
    */
   response => {
     const res = response.data
 
-    // if the custom code is not 20000, it is judged as an error.
+    // 如果自定义代码不是20000，则判断为错误.
     if (res.code !== 20000) {
       Message({
         message: res.message || 'Error',
@@ -53,9 +53,9 @@ service.interceptors.response.use(
         duration: 5 * 1000
       })
 
-      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
+      // 50008:非法令牌；50012:其他客户端登录；50014:令牌过期；
       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-        // to re-login
+        // 重新登录
         MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
           confirmButtonText: 'Re-Login',
           cancelButtonText: 'Cancel',
